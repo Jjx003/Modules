239adff7ad0ff59fd8131c1f98b5c10489c9a7b9
Jeff Xu <jjx003@ucsd.edu>
2020-06-16 17:12:05 -0700

    Fixed bugs

--- MY_MODULES/New Tags.lua
+++ MY_MODULES/New Tags.lua
@@ -1,34 +1,29 @@
 local module = {};
 
+
 --[[
     Functions:
-
         - module:addTag(player, info)
         - moudle:getLastTag(player)
         - module:getTags(player)
         - module:combatReport(player)
         - module:reset()
-
     addTag(player, info):
         // Given an input dictionary that has keys of [TEMPLATE] (see TEMPLATE below), create a new tag for player.
         // Ex: addTag(playerA, {creator=playerB,...}
         //  This would create a tag indicating that playerB has a tag on playerA.
         RETURNS: combo // Number of times player has performed same action within "timeout" seconds
-
     getLastTag(player):
         // Given a player, retrieve last tag on the player.
         // This is generally used to determine who dealt the final blow to player
         RETURNS: tag
-
     getTags(player):
         // Given a player, retrieve all the tags (max at SETTINGS.MAX_TAGS) that are within SETTINGS.ARCHIVE_TIME 
         //  seconds of creation.
         RETURNS: {tag1, ..., tagn}
-
     combatReport(player):
         // Given a player, generate a combat report:
         RETURNS: {totalDamage = 32, individual = {'12345'=20, '9999'=12}}
-
     reset():
         // Resets the tags and damage trackers. Use this after the game ends so module can be recycled.
         RETURNS: NONE
@@ -44,7 +39,6 @@ local SETTINGS = {
 
 --[[
     REQUIRED INFO:
-
     1. Creator - cxcharlie
     2. Creation Time - tick() (Automatically set, do not remove)
     3. Combo - 3x (Automatic)
@@ -79,9 +73,24 @@ local function createTag(info, combo)
             newTag[MAPPING[index]] = value;
         end
         newTag[MAPPING.creationTime] = tick();
-        newTag[MAPPING.combo] = combo;
+		newTag[MAPPING.combo] = combo;
         return newTag;
     end
+end
+
+function deepcopy(orig)
+    local orig_type = type(orig)
+    local copy
+    if orig_type == 'table' then
+        copy = {}
+        for orig_key, orig_value in next, orig, nil do
+            copy[deepcopy(orig_key)] = deepcopy(orig_value)
+        end
+        setmetatable(copy, deepcopy(getmetatable(orig)))
+    else -- number, string, boolean, etc
+        copy = orig
+    end
+    return copy
 end
 
 function module:addTag(player, info)
@@ -97,15 +106,20 @@ function module:addTag(player, info)
         table.remove(playerTags, 1);
     end
 
-    -- This helps us compress tags if they're repeat attacks within reasonable amount of time
+	    -- This helps us compress tags if they're repeat attacks within reasonable amount of time
+	local info_original = info;
+    local info = deepcopy(info);
     local pointerPos = #playerTags;
     local decrements = 0;
     local now = tick();
+    local actual = info.damage;
+
     local combo = 1;
-    while pointerPos >= 1 and decrements < SETTINGS.MAX_SEARCH do
+	    while pointerPos >= 1 and decrements < SETTINGS.MAX_SEARCH do
         local current = playerTags[pointerPos];
         if current[MAPPING.creator] == info.creator and current[MAPPING.type2] == info.type2 and
                 now - current[MAPPING.creationTime] < current[MAPPING.timeout] then
+            
             info.damage = info.damage + current[MAPPING.damage];
             combo = current[MAPPING.combo] + 1;
             table.remove(playerTags, pointerPos);
@@ -118,15 +132,15 @@ function module:addTag(player, info)
     table.insert(playerTags, createTag(info, combo));
 
     -- Keeping track of overall combat stats
-    local dStats = damageStats[info.creator]
+    local dStats = damageStats[info_original.creator]
     if dStats == nil then
-        damageStats[info.creator] = {};
-        dStats = damageStats[info.creator];
+        damageStats[info_original.creator] = {};
+        dStats = damageStats[info_original.creator];
         dStats[tostring(player.UserId)] = 0;
     end
 
     -- Increment total damage dealt
-    dStats[tostring(player.UserId)] = dStats[tostring(player.UserId)] + info.damage;
+    dStats[tostring(player.UserId)] = dStats[tostring(player.UserId)] + actual;
     return combo;
 end
 
@@ -145,20 +159,20 @@ end
 
 function module:getTags(player)
     local tags = tags[player];
-    if tags then
+	if tags then
         local now = tick();
-        local valid = {};
-        for _, v in pairs(tags) do
-            if now - v[MAPPING.creationTIme] < v[MAPPING.timeout] then
-                local verbose = {};
-                for index, name in pairs(MAPPING) do
-                    verbose[name] = v[index];
+		local valid = {};
+		for _, v in pairs(tags) do
+            if now - v[MAPPING.creationTime] < v[MAPPING.timeout] then
+				local verbose = {};
+                for index, name in pairs(TEMPLATE) do
+                    verbose[name] = v[index]
                 end
                 table.insert(valid, verbose);
             end
         end
         return valid;
-    end
+	end
     return nil;
 end
 
@@ -174,7 +188,7 @@ function module:combatReport(player)
         report.totalDamage = totalDamage;
         report.individual = data;
         return report;
-    end
+	end
     return nil;
 end
 
